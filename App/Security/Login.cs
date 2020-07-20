a4d6e74487074bf4750ca0ed6db2e8d14ed7bb7c
Bonifacio Juarez Ceja <bonifacio@revan-HP.reb>
2020-07-20 01:55:48 -0500

    Creacion de Seguridad

---
+++ App/Security/Login.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using App.MidlewareError;
+using Domain.entity;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace App.Security
+{
+    public class Login
+    {
+       public class Ejected : IRequest<UserData> {
+           public string Email{ get; set; }
+           public string password { get; set; }
+
+        }
+        public class EjectedValidation :AbstractValidator<Ejected>{
+            public EjectedValidation(){
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.password).NotEmpty();
+                RuleFor(x => x.Email).EmailAddress();
+            }
+        }
+        public class Handler : IRequestHandler<Ejected, UserData>
+        {
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+            public Handler(UserManager<User> manager, SignInManager<User> signInManager){
+                _userManager = manager;
+                _signInManager = signInManager;
+            }
+            public async Task<UserData> Handle(Ejected request, CancellationToken cancellationToken)
+            {
+                var usuario = await _userManager.FindByEmailAsync(request.Email);
+                if(usuario == null){
+                    throw new HandlerError(HttpStatusCode.Unauthorized);
+                }
+                var result = await _signInManager.CheckPasswordSignInAsync(usuario, request.password,false);
+                if(result.Succeeded){
+                    return new UserData
+                    {
+                        name = usuario.name,
+                        last_name = usuario.las_name,
+                        Token = "Token",
+                        Email = usuario.Email,
+                        Username = usuario.UserName,
+                        Image = null
+                    };
+                }
+                throw new HandlerError(HttpStatusCode.Unauthorized);
+            }   
+        }
+    }
+}
---
+++ App/Security/UserData.cs
@@ -0,0 +1,12 @@
+namespace App.Security
+{
+    public class UserData
+    {
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Username{ get; set; }
+        public string Image { get; set; }
+    }
+}
